5a88ca49fb2a494b2ff3656992831e1871b1ea0a
fg51 <zknsftk1msrk@gmail.com>
2023-11-02 13:57:51 +0900

    Fixed

--- cs-cpp/nativelib/build.rs
+++ cs-cpp/nativelib/build.rs
@@ -2,6 +2,6 @@ fn main() {
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("nativelib")
-        .generate_csharp_file("NativeMethods.g.cs")
+        .generate_csharp_file("../NativeMethods.g.cs")
         .unwrap();
 }
